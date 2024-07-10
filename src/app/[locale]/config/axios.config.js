17f691fbb037d9fb29a752686bee35afbb09f7bd
Rashadhusien <rashadhusein440@gmail.com>
2024-07-10 23:48:51 +0300

    fix service ar

--- src/app/[locale]/config/axios.config.js
+++ src/app/[locale]/config/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// http://localhost:1337/api
+//
 const axiosInstance = axios.create({
-  baseURL: "https://passionate-pleasure-95df767cc9.strapiapp.com/api",
+  baseURL: "http://localhost:1337/api",
   timeout: 1000,
 });
 
