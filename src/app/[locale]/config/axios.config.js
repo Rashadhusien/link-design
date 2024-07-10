9d6fe290e55c8815b50b88cce3cc8aea34f8fac4
Rashadhusien <rashadhusein440@gmail.com>
2024-07-10 23:01:51 +0300

    update api

--- src/app/[locale]/config/axios.config.js
+++ src/app/[locale]/config/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+// http://localhost:1337/api
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://passionate-pleasure-95df767cc9.strapiapp.com/api",
   timeout: 1000,
 });
 
